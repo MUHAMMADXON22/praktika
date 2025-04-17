e482bdd71e147e545318fa70d107e9210107d265
MUKHAM <muxammadxon994@gmail.com>
2025-04-17 11:08:39 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+
+const userColor = prompt("Введите цвет для фона :");
+if (userColor) ;
+const textColor = prompt("Введите цвет для текста:");
+if (textColor) ;
+document.body.style.backgroundColor = userColor;
+document.body.style.color = textColor;
